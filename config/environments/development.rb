2b44dccf488eb80c36849c336f7edd53ef3205c4
Sameer Rahmani <lxsameer@gnu.org>
2014-11-01 20:09:02 +0330

    Rspec and mongoid added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Mongoid.logger.level = Logger::DEBUG
+  Moped.logger.level = Logger::DEBUG
+
 end
